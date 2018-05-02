261d252b50dd40b944f30eac058211bf78b35abf
Ross McDonald <ross@stoplight.io>
2018-05-02 14:07:34 -0500

    Correct comments in rpm rebuild script

--- scripts/stoplight-rebuild-rpm.sh
+++ scripts/stoplight-rebuild-rpm.sh
@@ -23,8 +23,8 @@ cd gitlabhq && ./scripts/stoplight-bootstrap.sh
 
 echo "Now run: rpmrebuild -e gitlab-ce"
 
-# When editing the RPM, remember to rename the package and remove the
-# files.
+# When editing the RPM, remember to rename the package from gitlab-ce
+# to stoplight-gitlab.
 #
 # The last time I did this, I had to remove lines 69 - 78959 (or
 # everything under '%files').
@@ -34,3 +34,10 @@ echo "Now run: rpmrebuild -e gitlab-ce"
 # - going to line 69 with '69G'
 # - removing lines up to the one we want with 'd78959G'
 #
+# Once all of the existing files entries are removed, simply add a
+# single entry:
+#
+# /opt/gitlab
+#
+# Be sure that no %dir directive is set, as that will only include the
+# directory itself and not everything inside of it.
