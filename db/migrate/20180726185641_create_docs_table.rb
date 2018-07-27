dab8da6afbe0ac817af2e0d102a023039b46b092
Tom Pytleski <pytlesk4@gmail.com>
2018-07-27 15:57:34 -0500

    - add create docs migration   - adds docs table   - adds doc_builds table   - add trigger for setting/updating timestamps created_at on insert and updated_at on insert and update

---
+++ db/migrate/20180726185641_create_docs_table.rb
@@ -0,0 +1,126 @@
+# 20180726185641_create_docs_table.rb
+
+# rubocop:disable all
+class CreateDocsTable < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  def up
+    create_table :docs do |t|
+      t.integer :org_id
+      t.integer :project_id
+      t.integer :live_build_id
+      t.string :hostname
+      t.string :ssl_path
+      t.boolean :custom_ssl
+
+      # config
+          # integration
+            # segment
+            # intercom
+            # ga
+          # auth
+            # default
+            # basic
+            # saml
+            # auth0
+      t.json :config
+
+      t.timestamps null: true
+    end
+
+    create_table :doc_builds do |t|
+      t.integer :doc_id
+      t.string :ref
+      t.string :file_path
+      t.string :branch
+
+      # status
+        # code
+        # message
+      t.json :status
+
+      # config
+          # integration
+            # segment
+            # intercom
+            # google_analytics
+          # auth
+            # default
+            # basic
+            # saml
+            # auth0
+      t.json :config
+
+      t.timestamps null: true
+    end
+
+    if Gitlab::Database.postgresql?
+      execute %q{
+        ALTER TABLE "doc_builds"
+          ADD CONSTRAINT "doc_builds_doc_id_fkey"
+          FOREIGN KEY ("doc_id")
+          REFERENCES "docs" ("id")
+          ON DELETE CASCADE
+          NOT VALID;
+      }
+
+      ActiveRecord::Base.transaction do
+        execute <<-EOF
+            CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+            RETURNS trigger AS
+            $BODY$
+            BEGIN
+              IF NEW.created_at IS NULL then
+                NEW.created_at = NOW();
+              END IF;
+
+              NEW.updated_at = NOW();
+              RETURN NEW;
+            END;
+            $BODY$
+            LANGUAGE 'plpgsql'
+            VOLATILE;
+
+            CREATE TRIGGER trigger_set_timestamp
+            BEFORE INSERT OR UPDATE ON docs
+            FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+
+            CREATE TRIGGER trigger_set_timestamp
+            BEFORE INSERT OR UPDATE ON doc_builds
+            FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+        EOF
+      end
+    else
+      execute %q{
+        ALTER TABLE doc_builds
+          ADD CONSTRAINT doc_builds_doc_id_fkey
+          FOREIGN KEY (doc_id)
+          REFERENCES docs (id)
+          ON DELETE CASCADE;
+      }
+    end
+
+    # docs indexes
+    add_index "docs", ["org_id"], name: "index_docs_on_org_id", using: :btree
+    add_index "docs", ["hostname"], name: "index_docs_on_hostname", using: :btree, unique: true
+    add_index "docs", ["project_id"], name: "index_docs_on_project_id", using: :btree
+
+    # doc_builds indexes
+    add_index "doc_builds", ["doc_id"], name: "index_doc_builds_on_doc_id", using: :btree
+  end
+
+  def down
+    drop_table :doc_builds
+    drop_table :docs
+
+    if Gitlab::Database.postgresql?
+      execute <<-EOF
+        DROP TRIGGER IF EXISTS trigger_set_timestamp ON docs;
+        DROP TRIGGER IF EXISTS trigger_set_timestamp ON doc_builds;
+        DROP FUNCTION IF EXISTS trigger_set_timestamp();
+      EOF
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710120850) do
+ActiveRecord::Schema.define(version: 20180726185641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
