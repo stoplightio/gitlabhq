bb123a7ef19198604cfa84666eca163f865bedf8
Vazha Omanashvili <rainum.ua@gmail.com>
2018-08-17 16:39:47 +0300

    add postgress functions and triggers to a posts and comments migration to set iid and update created_at/updated_at timestamps

--- db/migrate/20180816115030_create_posts.rb
+++ db/migrate/20180816115030_create_posts.rb
@@ -1,4 +1,6 @@
 class CreatePosts < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
   class Post < ActiveRecord::Base
     enum state: [:open, :closed]
     enum type: [:discussion, :issue]
@@ -6,7 +8,7 @@ class CreatePosts < ActiveRecord::Migration
 
   DOWNTIME = false
 
-  def change
+  def up
     create_table :posts do |t|
       t.integer :iid, null: false
       t.integer :project_id, null: false
@@ -19,19 +21,73 @@ class CreatePosts < ActiveRecord::Migration
       t.string :body
       t.string :file_loc
 
-      t.datetime_with_timezone :created_at, null: false
-      t.datetime_with_timezone :updated_at, null: false
+      t.datetime_with_timezone :created_at
+      t.datetime_with_timezone :updated_at
 
       t.foreign_key :projects, column: :project_id, on_delete: :cascade
       t.foreign_key :users, column: :creator_id, on_delete: :cascade
       # TODO: uncomment this after files migrations will be implemented
       # t.foreign_key :files, column: :file_id, on_delete: :cascade
       
-      t.index :project_id
+      t.index [:project_id, :iid], unique: true
       t.index :file_id
       t.index :creator_id
       t.index :state
       t.index :created_at
+    end
+    
+    if Gitlab::Database.postgresql?
+      execute <<-SQL
+        CREATE OR REPLACE FUNCTION trigger_set_post_timestamp()
+        RETURNS trigger AS
+        $BODY$
+        BEGIN
+            IF NEW.created_at IS NULL then
+                NEW.created_at = NOW();
+            END IF;
+
+            NEW.updated_at = NOW();
+            RETURN NEW;
+        END;
+        $BODY$
+        LANGUAGE 'plpgsql'
+        VOLATILE;
+
+        CREATE OR REPLACE FUNCTION trigger_set_post_iid()
+        RETURNS trigger AS
+        $BODY$
+        BEGIN
+            SELECT COALESCE(MAX(iid) + 1, 1)
+            INTO NEW.iid
+            FROM posts
+            WHERE project_id = NEW.project_id;
+
+            RETURN NEW;
+        END;
+        $BODY$
+        LANGUAGE 'plpgsql'
+        VOLATILE;
+
+        CREATE TRIGGER trigger_set_post_iid
+        BEFORE INSERT OR UPDATE ON posts
+        FOR EACH ROW EXECUTE PROCEDURE trigger_set_post_iid();
+        
+        CREATE TRIGGER trigger_set_post_timestamp
+        BEFORE INSERT OR UPDATE ON posts
+        FOR EACH ROW EXECUTE PROCEDURE trigger_set_post_timestamp();
+      SQL
+    end
+  end
+
+  def down
+    drop_table :posts
+
+    if Gitlab::Database.postgresql?
+      execute <<-SQL
+        DROP TRIGGER IF EXISTS trigger_set_post_iid ON posts;
+        DROP TRIGGER IF EXISTS trigger_set_post_timestamp ON posts;
+        DROP FUNCTION IF EXISTS trigger_set_post_iid();
+      SQL
     end
   end
 end
--- db/migrate/20180816143435_create_comments.rb
+++ db/migrate/20180816143435_create_comments.rb
@@ -1,11 +1,13 @@
 class CreateComments < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
   class Comment < ActiveRecord::Base
     enum parent_type: [:post, :commit, :pull_request, :build, :version, :release]
   end
 
   DOWNTIME = false
 
-  def change
+  def up
     create_table :comments do |t|
       t.integer :creator_id, null: false
       t.integer :parent_id, null: false
@@ -17,6 +19,39 @@ class CreateComments < ActiveRecord::Migration
 
       t.index [:parent_type, :parent_id]
       t.index :created_at
+    end
+
+    if Gitlab::Database.postgresql?
+      execute <<-SQL
+        CREATE OR REPLACE FUNCTION trigger_set_comment_timestamp()
+        RETURNS trigger AS
+        $BODY$
+        BEGIN
+            IF NEW.created_at IS NULL then
+                NEW.created_at = NOW();
+            END IF;
+
+          NEW.updated_at = NOW();
+          RETURN NEW;
+        END;
+        $BODY$
+        LANGUAGE 'plpgsql'
+        VOLATILE;
+
+        CREATE TRIGGER trigger_set_comment_timestamp
+        BEFORE INSERT OR UPDATE ON comments
+        FOR EACH ROW EXECUTE PROCEDURE trigger_set_comment_timestamp();
+      SQL
+    end
+  end
+
+  def down
+    drop_table :comments
+
+    if Gitlab::Database.postgresql?
+      execute <<-SQL
+        DROP TRIGGER IF EXISTS trigger_set_comment_timestamp ON comments;
+      SQL
     end
   end
 end
