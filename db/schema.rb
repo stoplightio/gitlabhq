b0b770d1f1197f693ad5739bdaba7c741bb66470
Thomas Pytleski <pytlesk4@gmail.com>
2019-03-01 15:41:15 -0500

    SL-1951 Update indexes on node_version_history_changelog table (#18)
    * fix: update node_version_history_changelog indexes
    
    [SL-1951](https://stoplightio.atlassian.net/browse/SL-1951)
    
    * chore: add schema
    
    [SL-1951](https://stoplightio.atlassian.net/browse/SL-1951)
    
    * Update 20190301202346_update_node_version_history_changelog_indexes.rb
    
    fix typeo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190219173231) do
+ActiveRecord::Schema.define(version: 20190301202346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1473,6 +1473,8 @@ ActiveRecord::Schema.define(version: 20190219173231) do
     t.text     "message",                                   null: false
     t.datetime "created_at",              default: "now()", null: false
   end
+
+  add_index "node_version_history_changelog", ["node_version_history_id", "message"], name: "node_version_history_changelog_node_version_hisotry_id_message_", unique: true, using: :btree
 
   create_table "node_version_history_validations", force: :cascade do |t|
     t.integer  "node_version_history_id",                   null: false
