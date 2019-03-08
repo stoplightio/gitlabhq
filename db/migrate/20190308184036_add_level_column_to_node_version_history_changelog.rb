68af8895715354bbaec45abf3d6aa7f1865987d6
Thomas Pytleski <pytlesk4@gmail.com>
2019-03-08 14:53:47 -0500

    Add level column (#20)
    * add level column to node version history changelog
    
    * fix error with incorrect migration name
    
    * update shema

---
+++ db/migrate/20190308184036_add_level_column_to_node_version_history_changelog.rb
@@ -0,0 +1,19 @@
+# 20190308184036_add_level_column_to_node_version_history_changelog.rb
+
+class AddLevelColumnToNodeVersionHistoryChangelog < ActiveRecord::Migration
+  include Gitlab::Database::MigrationHelpers
+
+  DOWNTIME = false
+
+  def up
+    if Gitlab::Database.postgresql?
+      execute <<-SQL
+        ALTER TABLE node_version_history_changelog ADD COLUMN level int4 NOT NULL;
+      SQL
+    end
+  end
+
+  def down
+
+  end
+end
